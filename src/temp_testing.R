ed28c5579a60aba6f11d0f606dd8b27f66996b50
T. Ryan Johnson <johnsontr@gmail.com>
2020-04-03 19:39:36 -0500

    Update temp_testing.R
    Update temp_testing to match the contents of the verification.pdf to make sure the density wrapper is working.

--- src/temp_testing.R
+++ src/temp_testing.R
@@ -2,20 +2,31 @@
 # Contact: thomas.johnson@wustl.edu
 # Date: April 3, 2020
 
-###
-### Testing
-###
-# df <- dgen.simpleLinear()
+# Make the data set. Binary Outcome Y, Binary Treament Indicator
 df <- dgen.rbinom()
 
-posterior <- posterior.theta(df$Y, df$D)
+# Posterior distribution of the model parameters when Y = intercept + ATE*D + u
+posterior.theta(df$Y, df$D)
 
+# Posterior predictive distribution for the binary outcome when d=0
 posterior.c <- posterior.predictive.outcome(df$Y, df$D,  d=0, sigma0=1, lambda=1)
+# Posterior predictive distribution for the binary outcome when d=1
 posterior.t <- posterior.predictive.outcome(df$Y, df$D,  d=1, sigma0=1, lambda=1)
 
-ppo.density(y=1, posterior.c)
+# Posterior predictive distribution for the binary outcome when d=0
+posterior.c 
+
+# Posterior predictive distribution for the binary outcome when d=1
+posterior.t 
+
+# dnorm using the output from posterior.predictive.outcome when d=0 and y=0
 ppo.density(y=0, posterior.c)
-ppo.density(y=1, posterior.t)
+
+# dnorm using the output from posterior.predictive.outcome when d=0 and y=1
+ppo.density(y=1, posterior.c)
+
+# dnorm using the output from posterior.predictive.outcome when d=1 and y=0
 ppo.density(y=0, posterior.t)
 
-
+# dnorm using the output from posterior.predictive.outcome when d=1 and y=1
+ppo.density(y=1, posterior.t)

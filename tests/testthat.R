26f800553c7e05af91a0b452de1e1ac2df813581
T. Ryan Johnson <johnsontr@gmail.com>
2020-05-05 11:19:23 -0500

    Rename function files and begin unit testing
    Renamed eqnXX.R files to match the function name contained within. This matches standards from r-pkgs.org. I started unit testing, but I don't know how to unit test a data generating process like dgenRbinom and dgenSimpleLinear.

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(entmax)
+
+test_check("entmax")
---
+++ tests/testthat/test-dgenRbinom.R
@@ -0,0 +1,6 @@
+test_that("dgenRbinom() generates a data frame with binary outcome, binary treatment indicator, and num.obs total observations", {
+
+  # How do I make a test for a data generating function???
+
+  expect_equal(2 * 2, 4)
+})
